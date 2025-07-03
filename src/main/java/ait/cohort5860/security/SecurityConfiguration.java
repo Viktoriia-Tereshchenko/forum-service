53152ab9e5c285c9d31954bbb16f184e263659df
ViktoriiaTereshchenko <vika.s.tereshchenko@gmail.com>
2025-07-03 12:24:00 +0200

    complete cors

--- src/main/java/ait/cohort5860/security/SecurityConfiguration.java
+++ src/main/java/ait/cohort5860/security/SecurityConfiguration.java
@@ -13,6 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 // Realization of AUTHORIZATION
 
@@ -29,28 +33,31 @@ public class SecurityConfiguration {
         // each request must have an AUTHORIZATION header
         http.httpBasic(Customizer.withDefaults()); // // Basic Auth enabled
         http.csrf(csrf -> csrf.disable()); // csrf
+
+        http.cors(Customizer.withDefaults());
+
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/account/register", "forum/posts/**")
-                    .permitAll() // anyone can register
+                .permitAll() // anyone can register
                 .requestMatchers(("/account/user/{login}/role/{role}"))
-                    .hasRole(Role.ADMINISTRATOR.name()) // only ADMINISTRATOR
+                .hasRole(Role.ADMINISTRATOR.name()) // only ADMINISTRATOR
                 .requestMatchers(HttpMethod.PATCH, "/account/user/{login}", "/forum/post/{id}/comment/{login}")
-                    .access(new WebExpressionAuthorizationManager("#login == authentication.name")) // only owner
+                .access(new WebExpressionAuthorizationManager("#login == authentication.name")) // only owner
                 .requestMatchers(HttpMethod.DELETE, "/account/user/{login}")
-                    .access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')")) // only owner or ADMINISTRATOR
+                .access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')")) // only owner or ADMINISTRATOR
                 .requestMatchers(HttpMethod.POST, "/forum/post/{author}")
-                    .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+                .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
                 .requestMatchers(HttpMethod.PATCH, "/forum/post/{id}")
-                    .access(((authentication, context) ->
+                .access(((authentication, context) ->
                         new AuthorizationDecision(webSecurity.checkPostAuthor(context.getVariables().get("id"), authentication.get().getName()))))
                 .requestMatchers(HttpMethod.DELETE, "/forum/post/{id}")
-                    .access((authentication, context) -> {
+                .access((authentication, context) -> {
                     boolean isAuthor = webSecurity.checkPostAuthor(context.getVariables().get("id"), authentication.get().getName());
                     boolean isModerator = context.getRequest().isUserInRole(Role.MODERATOR.name());
                     return new AuthorizationDecision(isAuthor || isModerator);
                 })
                 .anyRequest()
-                    .authenticated()); // authentication is required
+                .authenticated()); // authentication is required
         //.permitAll()); // allowed to all
         // request without header
         // or with header but correct (correct login and password)
@@ -67,5 +74,17 @@ public class SecurityConfiguration {
     @Bean
     PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // с любого сайта
+        // но мы должны указать адрес нашего фронтэнда
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*")); // все заголовки
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
